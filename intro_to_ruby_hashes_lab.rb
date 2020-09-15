d8d18f5b19437f07cbad8d6d9f434de1cb2391f8
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-15 22:35:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
 	        :three_pieces_owned => 100
-	        :four_peices_owned => 200
+	        :four_pieces_owned => 200
 	      }
 	  }
 	  
