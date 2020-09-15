de11bd3017b9d410556a5d8374930af2114ab297
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-15 22:47:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,11 @@ def monopoly_with_third_tier
 	  {:railroads => 
 	    {
 	      :pieces => 4,
-	      :names => {},
+	      :names => 
+	        {
+	          :reading_railroad => {},
+	          :pennsylvania_railroad => {}
+	        },
 	      :rent_in_dollars => 
 	        {
 	          :one_piece_owned => 25,
